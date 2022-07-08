d60c8abf47324c2309a512e4e0c4c7c4b7716ab4
lenovo <lenovo@LAPTOP-BLNTEBH7>
2022-07-08 09:40:25 +0530

    This is the data for the redux

---
+++ src/action/index.js
@@ -0,0 +1,12 @@
+import { Decrement, Increment } from "../constant";
+
+export const IncrementAction = () => ({
+  type: Increment,
+  payload:"ddd"
+  
+})
+
+export const DecrementAction = () => ({
+    type: Decrement,
+    
+  })
